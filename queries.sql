7c41dca036c28ce034e4520bcc0c3563c3ca290e
Zaw Wunna <zawwunna@Zaws-Air.lan>
2023-08-12 15:04:35 -0700

    adding files

---
+++ queries.sql
@@ -0,0 +1,15 @@
+-- write your queries here
+
+-- First Query
+SELECT * FROM owners o FULL OUTER JOIN vehicles v ON o.id = v.owner_id;
+
+--Second Query
+SELECT first_name, last_name, COUNT(owner_id) 
+FROM owners o JOIN vehicles v on o.id = v.owner_id
+GROUP BY (first_name, last_name) ORDER BY first_name;
+
+--Third Query
+SELECT first_name, last_name, ROUND(AVG(price)) as avg_price,
+COUNT(owner_id) FROM owners o JOIN vehicles v on o.id = v.owner_id
+GROUP BY (first_name, last_name) HAVING COUNT(owner_id) > 1
+AND ROUND(AVG(price)) > 10000 ORDER BY first_name DESC;
